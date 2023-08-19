bb9b5118830cc116dfa0547df8be70fcdafc4dc5
Lucho <antonio.saurith@gmail.com>
2023-08-19 16:15:32 -0400

    eliminando node_modules

--- 03-fundamentos-node/helpers/multiplicar.js
+++ 03-fundamentos-node/helpers/multiplicar.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 const colors = require('colors');
-
 const crearArchivo = async (base, listar, hasta) => {
     try {
         let salida = '';
         for (let index = 1; index <= hasta; index++) {
             salida += `${base} x ${index} = ${base * index}\n`;
         }
-        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
         if(listar) {
             console.log('===================='.rainbow.bgBlack)
             console.log(`Tabla del ${base}`);
             console.log('===================='.rainbow.bgBlack);
             console.log(salida.rainbow);
         }
-        return `Tabla-${base}.txt`.bgWhite;
+        return `tabla-${base}.txt`.bgWhite;
     } catch (error) {
         throw error;
     }
